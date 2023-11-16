ae1d447689713ee4887ba5bb315c99719d04727a
arthursuzuki <126833330+arthursuzuki@users.noreply.github.com>
2023-11-16 18:36:14 +0000

    creating all app routes and lofi from home.html

--- app.py
+++ app.py
@@ -3,41 +3,52 @@ from flask.templating import render_template_string
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
-    return render_template('home.html')
-  
+  return render_template('home.html')
+
+
 @app.route('/atualizacao')
 def atualizacao():
-    return render_template('atualizacao.html')
-  
+  return render_template('atualizacao.html')
+
+
 @app.route('/consulta')
 def consulta():
-    return render_template('consulta.html')
-  
+  return render_template('consulta.html')
+
+
 @app.route('/demo')
 def demo():
-    return render_template('demo.html')
-  
+  return render_template('demo.html')
+
+
 @app.route('/documentacao')
-def demo():
+def documentacao():
   return render_template('documentacao.html')
-  
+
+
 @app.route('/erros')
 def erros():
   return render_template('erros.html')
-  
+
+
 @app.route('/exclusao')
 def exclusao():
   return render_template('exclusao.html')
-  
+
+
 @app.route('/incersao')
 def incersao():
   return render_template('incersao.html')
 
+
 @app.route('/relatorios')
 def relatorios():
   return render_template('relatorios.html')
+
+
 @app.route('/testes')
 def testes():
   return render_template('testes.html')
