073b4c87ee1f3d724929a5bf8d18973a039b955f
arthursuzuki <126833330+arthursuzuki@users.noreply.github.com>
2023-11-16 17:57:01 +0000

    adding all app routes

--- app.py
+++ app.py
@@ -1,10 +1,47 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask.templating import render_template_string
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return render_template('home.html')
+  
+@app.route('/atualizacao')
+def atualizacao():
+    return render_template('atualizacao.html')
+  
+@app.route('/consulta')
+def consulta():
+    return render_template('consulta.html')
+  
+@app.route('/demo')
+def demo():
+    return render_template('demo.html')
+  
+@app.route('/documentacao')
+def demo():
+  return render_template('documentacao.html')
+  
+@app.route('/erros')
+def erros():
+  return render_template('erros.html')
+  
+@app.route('/exclusao')
+def exclusao():
+  return render_template('exclusao.html')
+  
+@app.route('/incersao')
+def incersao():
+  return render_template('incersao.html')
+
+@app.route('/relatorios')
+def relatorios():
+  return render_template('relatorios.html')
+@app.route('/testes')
+def testes():
+  return render_template('testes.html')
+
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', port=8080, debug=True)
