5a3e65f9939c7d633f3c8471b082b7efb25f48f4
Julio H Cavalcanti <juliophc@msn.com>
2023-11-18 14:14:29 -0300

    feat: inserção prof e aluno

--- main.py
+++ main.py
@@ -15,6 +15,30 @@ mydb = mysql.connector.connect(
 cursor = mydb.cursor()
 print("conexão efetivada com sucesso")
 
+def db_insert_prof(nome, email):
+    sql = f"INSERT INTO Usuario (nome, email) VALUES ('{nome}', '{email}');"
+    cursor.execute(sql)
+
+    cursor.fetchall()
+
+    sql = f"INSERT INTO Professor (id) VALUES (LAST_INSERT_ID());"
+    cursor.execute(sql)
+
+    mydb.commit()
+    st.success("Record Created Successfully!!!")
+
+def db_insert_aluno(nome, email, ano_escolar):
+    sql = f"INSERT INTO Usuario (nome, email) VALUES ('{nome}', '{email}');"
+
+    cursor.execute(sql)
+
+    cursor.fetchall()
+
+    sql = f"INSERT INTO Aluno (id, ano_escolar) VALUES ((SELECT LAST_INSERT_ID()), {ano_escolar});"
+    cursor.execute(sql)
+
+    mydb.commit()
+    st.success("Record Created Successfully!!!")
 
 def db_select_aluno_prof(table):
     sql = f"SELECT Usuario.id, Usuario.email, Usuario.nome FROM USUARIO INNER JOIN {table} ON Usuario.id = {table}.id"
@@ -31,6 +55,7 @@ def db_select(table):
 
 
 def main():
+    
     st.title("CRUD operações bd da rede social")
     option = st.sidebar.selectbox('Selecione uma operação', ('Visualizar', 'Inserir', 'Alterar', 'Deletar', ))
     table = st.sidebar.selectbox('Selecione uma tabela', ('Professor', 'Aluno', 'Grupo', 'Escola', 'Disciplina', 'Olimpiada', 'Post', 'Comentarios', 'Grupo_Disciplina', 'Prof_Disciplina', 'Leciona', 'Assiste', 'Concorre', 'Olimpiada_Disciplina'))
@@ -46,11 +71,27 @@ def main():
         st.write(f"Dados da Tabela {table}")
         for row in result:
             st.write(row)
+        cursor = mydb.cursor()
 
     elif option == "Inserir":
         st.subheader("Inserir um dado a uma tabela")
-        st.text_input("Insira um nome")
-        st.text_input("Insira o email")
+        if table == "Professor":
+            nome = st.text_input("Nome do Professor")
+            email = st.text_input("Email do Professor")
+            
+            if st.button("Inserir"):
+                db_insert_prof(nome, email)
+        
+        elif table == "Aluno":
+            nome = st.text_input("Nome do Aluno")
+            email = st.text_input("Email do Aluno")
+            ano_escolar = st.number_input("Ano Escolar")
+
+            if st.button("Inserir"):
+                db_insert_aluno(nome, email, ano_escolar)
+
+
+            
 
     elif option == "Alterar":
         st.subheader("Alterar dado em tabela")
