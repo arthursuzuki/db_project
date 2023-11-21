e3ac15ccf3239ccb2212806f66364aee07863fc7
suzuki <ajls@cesar.school>
2023-11-21 11:55:35 -0300

    taking off comments

--- main.py
+++ main.py
@@ -364,6 +364,7 @@ def main():
                 st.success("Atualizado com SUCESSO!")
 
 
+
         elif table == "Escola":
             id = st.number_input("Id da Escola", min_value=1)
             nome = st.text_input("nome da Escola")
@@ -439,11 +440,6 @@ def main():
                 cursor.execute(sql)
                 mydb.commit()
                 st.success("Atualizado com SUCESSO!")
-<<<<<<< HEAD
-
-=======
-## ate aqui ta tudo ok
->>>>>>> 1ca837faa319d5fd33de2b94aaca9f67b6916266
 
         elif table == "Prof_Disciplina":
             fk_professor_id = st.number_input("Antigo Id do Professor", min_value=1)
@@ -608,13 +604,8 @@ def main():
                 cursor.execute(sql)
                 mydb.commit()
                 st.success("Deletado com SUCESSO!") 
-<<<<<<< HEAD
- 
-=======
 
-######
     
->>>>>>> 1ca837faa319d5fd33de2b94aaca9f67b6916266
         elif table == "Leciona":
             fk_professor_id = st.number_input("Id do professor", min_value= 1)
             fk_escola_id = st.number_input("Id da escola", min_value= 1)
