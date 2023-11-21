d9a39717d2a8eab752bb2ca365851530a01afc6c
Julio H Cavalcanti <juliophc@msn.com>
2023-11-21 11:46:43 -0300

    fix: traducao de sucess msg

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ def db_insert_prof(nome, email):
     cursor.execute(sql)
 
     mydb.commit()
-    st.success("Record Created Successfully!!!")
+    st.success("Criado com SUCESSO!")
 
 def db_insert_aluno(nome, email, ano_escolar, id_escola):
     sql = f"INSERT INTO Usuario (nome, email) VALUES ('{nome}', '{email}');"
@@ -37,13 +37,13 @@ def db_insert_aluno(nome, email, ano_escolar, id_escola):
     cursor.execute(sql)
 
     mydb.commit()
-    st.success("Record Created Successfully!!!")
+    st.success("Criado com SUCESSO!")
 
 def db_insert_grupo(nome, descricao, fk_professor_id):
     sql = f"INSERT INTO Grupo (nome, descricao, fk_professor_id) VALUES ('{nome}', '{descricao}', {fk_professor_id});"
     cursor.execute(sql)
     mydb.commit()
-    st.success("Grupo Record Created Successfully!!!")
+    st.success("Criado com SUCESSO!")
 
 def db_insert_escola(nome, tipo):
     sql_usuario = f"INSERT INTO Usuario (nome) VALUES ('{nome}');"
@@ -53,81 +53,81 @@ def db_insert_escola(nome, tipo):
     sql_escola = f"INSERT INTO Escola (nome, tipo) VALUES ('{nome}', '{tipo}');"
     cursor.execute(sql_escola)
     mydb.commit()
-    st.success("Record Created Successfully!!!")
+    st.success("Criado com SUCESSO!")
 
 def db_insert_disciplina(nome):
     sql = f"INSERT INTO Disciplina (nome) VALUES ('{nome}');"
     cursor.execute(sql)
     mydb.commit()
-    st.success("Disciplina Record Created Successfully!!!")
+    st.success("Criado com SUCESSO!")
 
 def db_insert_participa(fk_usuario_id, fk_grupo_id):
     sql = f"INSERT INTO participa (fk_usuario_id, fk_grupo_id) VALUES ({fk_usuario_id}, {fk_grupo_id});"
     cursor.execute(sql)
     mydb.commit()
-    st.success("Participa Record Created Successfully!!!")
+    st.success("Criado com SUCESSO!")
 
 def db_insert_olimpiada(nome, estado, cidade, rua, numero, edicao):
     sql = f"INSERT INTO Olimpiada (nome, estado, cidade, rua, numero, edicao) VALUES ('{nome}', '{estado}', '{cidade}', '{rua}', '{numero}', {edicao});"
     cursor.execute(sql)
     mydb.commit()
-    st.success("Olimpiada Record Created Successfully!!!")
+    st.success("Criado com SUCESSO!")
 
 def db_insert_post(conteudo, data_post, fk_usuario_id):
     sql = f"INSERT INTO Post (conteudo, data_post, fk_usuario_id) VALUES ('{conteudo}', '{data_post}', {fk_usuario_id});"
     cursor.execute(sql)
     mydb.commit()
-    st.success("Post Record Created Successfully!!!")
+    st.success("Criado com SUCESSO!")
 
 def db_insert_comentarios(conteudo, data_comentario, fk_comentarios_id, fk_post_id, fk_usuario_id):
     sql = f"INSERT INTO Comentarios (conteudo, data_comentario, fk_comentarios_id, fk_post_id, fk_usuario_id) VALUES ('{conteudo}', '{data_comentario}', {fk_comentarios_id}, {fk_post_id}, {fk_usuario_id});"
     cursor.execute(sql)
     mydb.commit()
-    st.success("Comentarios Record Created Successfully!!!")
+    st.success("Criado com SUCESSO!")
 
 def db_insert_grupo_disciplina(fk_grupo_id, fk_disciplina_id):
     sql = f"INSERT INTO grupo_disciplina (fk_grupo_id, fk_disciplina_id) VALUES ({fk_grupo_id}, {fk_disciplina_id});"
     cursor.execute(sql)
     mydb.commit()
-    st.success("Grupo Disciplina Record Created Successfully!!!")
+    st.success("Criado com SUCESSO!")
 
 
 def db_insert_participa(fk_usuario_id, fk_grupo_id):
     sql = f"INSERT INTO participa (fk_usuario_id, fk_grupo_id) VALUES ({fk_usuario_id}, {fk_grupo_id});"
     cursor.execute(sql)
     mydb.commit()
-    st.success("Participa Record Created Successfully!!!")
+    st.success("Criado com SUCESSO!")
 
 def db_insert_prof_disciplina(fk_professor_id, fk_disciplina_id):
     sql = f"INSERT INTO prof_disciplina (fk_professor_id, fk_disciplina_id) VALUES ({fk_professor_id}, {fk_disciplina_id});"
     cursor.execute(sql)
     mydb.commit()
-    st.success("Prof Disciplina Record Created Successfully!!!")
+    st.success("Criado com SUCESSO!")
 
 def db_insert_leciona(fk_professor_id, fk_escola_id):
     sql = f"INSERT INTO leciona (fk_professor_id, fk_escola_id) VALUES ({fk_professor_id}, {fk_escola_id});"
     cursor.execute(sql)
     mydb.commit()
-    st.success("Leciona Record Created Successfully!!!")
+    st.success("Criado com SUCESSO!")
 
 def db_insert_assiste(cursor, fk_aluno_id_assiste, fk_professor_id_assiste, fk_escola_id_assiste):
     sql = f"INSERT INTO assiste (fk_aluno_id, fk_professor_id, fk_escola_id) VALUES ({fk_aluno_id_assiste}, {fk_professor_id_assiste}, {fk_escola_id_assiste});"
     cursor.execute(sql)
     mydb.commit()
-    st.success("Leciona Record Created Successfully!!!")
+    st.success("Criado com SUCESSO!")
         
 
 def db_insert_concorre(fk_aluno_id, fk_olimpiada_id):
     sql = f"INSERT INTO concorre (fk_aluno_id, fk_olimpiada_id) VALUES ({fk_aluno_id}, {fk_olimpiada_id});"
     cursor.execute(sql)
     mydb.commit()
-    st.success("Concorre Record Created Successfully!!!")
+    st.success("Criado com SUCESSO!")
 
 def db_insert_olimpiada_disciplina(fk_olimpiada_id, fk_disciplina_id):
     sql = f"INSERT INTO olimpiada_disciplina (fk_olimpiada_id, fk_disciplina_id) VALUES ({fk_olimpiada_id}, {fk_disciplina_id});"
     cursor.execute(sql)
     mydb.commit()
-    st.success("Olimpiada Disciplina Record Created Successfully!!!")
+    st.success("Criado com SUCESSO!")
 
 
 def db_select_prof(table):
@@ -336,7 +336,7 @@ def main():
                 slq = f"UPDATE Usuario SET Usuario.nome = '{nome}', Usuario.email = '{email}' WHERE Usuario.id = {int(id)};"
                 cursor.execute(sql)
                 mydb.commit()
-                st.success("Record Updated Successfully!!!")
+                st.success("Atualizado com SUCESSO!")
 
         elif table == "Aluno":
             id = st.number_input("Id do Aluno", min_value= 1)
@@ -353,7 +353,7 @@ def main():
                 cursor.execute(sql)
 
                 mydb.commit()
-                st.success("Record Updated Successfully!!!")
+                st.success("Atualizado com SUCESSO!")
 
         elif table == 'Grupo':
             id = st.number_input("Id do Grupo", min_value =1)
@@ -364,7 +364,7 @@ def main():
                 sql = f"UPDATE Grupo SET Grupo.nome = '{nome}', Grupo.descricao = '{descricao}', Grupo.fk_professor_id = {int(fk_professor_id)} WHERE Grupo.id = {int(id)};"
                 cursor.execute(sql)
                 mydb.commit()
-                st.success("Record Updated Successfully!!!")
+                st.success("Atualizado com SUCESSO!")
 
 ####sss
 
@@ -376,7 +376,7 @@ def main():
                 sql = f"UPDATE Escola SET Escola.nome = '{nome}', Escola.tipo = '{tipo}' WHERE Escola.id = {int(id)};"
                 cursor.execute(sql)
                 mydb.commit()
-                st.success("Record Updated Successfully!!!")
+                st.success("Atualizado com SUCESSO!")
 
         elif table == "Disciplina":
             id = st.number_input("Id da Disciplina", min_value=1)
@@ -385,7 +385,7 @@ def main():
                 sql = f"UPDATE Disciplina SET Disciplina.nome = '{nome}' WHERE Disciplina.id = {int(id)};"
                 cursor.execute(sql)
                 mydb.commit()
-                st.success("Record Updated Successfully!!!")
+                st.success("Atualizado com SUCESSO!")
 
         elif table == "Olimpiada":
             id = st.number_input("Id da Olimpíada", min_value=1)
@@ -399,7 +399,7 @@ def main():
                 sql = f"UPDATE Olimpiada SET Olimpiada.nome = '{nome}', Olimpiada.estado = '{estado}', Olimpiada.cidade = '{cidade}', Olimpiada.rua = '{rua}', Olimpiada.numero = '{numero}', Olimpiada.edicao = {int(edição)} WHERE Olimpiada.id = {int(id)};"
                 cursor.execute(sql)
                 mydb.commit()
-                st.success("Record Updated Successfully!!!")
+                st.success("Atualizado com SUCESSO!")
 
         elif table == "Post":
             id = st.number_input("Id do Post", min_value=1)
@@ -409,7 +409,7 @@ def main():
                 sql = f"UPDATE Post SET Post.conteúdo = '{conteúdo}', Post.data_post = '{data_post}' WHERE Post.id = {int(id)};"
                 cursor.execute(sql)
                 mydb.commit()
-                st.success("Record Updated Successfully!!!")
+                st.success("Atualizado com SUCESSO!")
 
         elif table == "Comentarios":
             id = st.number_input("Id do Comentário", min_value=1)
@@ -419,7 +419,7 @@ def main():
                 sql = f"UPDATE Comentários SET Comentários.conteúdo = '{conteúdo}', Comentários.data_comentario = '{data_comentario}' WHERE Comentários.id = {int(id)};"
                 cursor.execute(sql)
                 mydb.commit()
-                st.success("Record Updated Successfully!!!")
+                st.success("Atualizado com SUCESSO!")
 
 
         elif table == "Participa":
@@ -431,7 +431,7 @@ def main():
                 sql = f"UPDATE Participa SET Participa.fk_usuario_id = {int(id_user_new)}, Participa.fk_grupo_id = {int(id_group_new)} WHERE Participa.fk_usuario_id = {int(id_user)} AND Participa.fk_grupo_id = {int(id_group)};"
                 cursor.execute(sql)
                 mydb.commit()
-                st.success("Record Updated Successfully!!!")
+                st.success("Atualizado com SUCESSO!")
 
         elif table == "Grupo_Disciplina":
             fk_grupo_id = st.number_input("Antigo Id do Grupo", min_value=1)
@@ -442,7 +442,7 @@ def main():
                 sql = f"UPDATE Grupo_Disciplina SET Grupo_Disciplina.fk_grupo_id = {int(fk_grupo_id_new)}, Grupo_Disciplina.fk_disciplina_id = {int(fk_disciplina_id_new)} WHERE Grupo_Disciplina.fk_grupo_id = {int(fk_grupo_id)} AND Grupo_Disciplina.fk_disciplina_id = {int(fk_disciplina_id)};"
                 cursor.execute(sql)
                 mydb.commit()
-                st.success("Record Updated Successfully!!!")
+                st.success("Atualizado com SUCESSO!")
 ## ate aqui ta tudo ok
 
         elif table == "Prof_Disciplina":
@@ -454,7 +454,7 @@ def main():
                 sql = f"UPDATE Prof_Disciplina SET Prof_Disciplina.fk_professor_id = {int(new_fk_professor_id)}, Prof_Disciplina.fk_disciplina_id = {int(new_fk_disciplina_id)} WHERE Prof_Disciplina.fk_professor_id = {int(fk_professor_id)} AND Prof_Disciplina.fk_disciplina_id = {int(fk_disciplina_id)};"
                 cursor.execute(sql)
                 mydb.commit()
-                st.success("Record Updated Successfully!!!")
+                st.success("Atualizado com SUCESSO!")
 
         elif table == "Leciona":
             fk_professor_id = st.number_input("Antigo Id do Professor", min_value=1)
@@ -465,7 +465,7 @@ def main():
                 sql = f"UPDATE Leciona SET Leciona.fk_professor_id = {int(new_fk_professor_id)}, Leciona.fk_escola_id = {int(new_fk_escola_id)} WHERE Leciona.fk_professor_id = {int(fk_professor_id)} AND Leciona.fk_escola_id = {int(fk_escola_id)};"
                 cursor.execute(sql)
                 mydb.commit()
-                st.success("Record Updated Successfully!!!")
+                st.success("Atualizado com SUCESSO!")
 
         elif table == "Assiste":
             fk_aluno_id = st.number_input("Antigo Id do Aluno", min_value=1)
@@ -478,7 +478,7 @@ def main():
                 sql = f"UPDATE Assiste SET Assiste.fk_aluno_id = {int(fk_aluno_id_new)}, Assiste.fk_professor_id = {int(fk_professor_id_new)}, Assiste.fk_escola_id = {int(fk_escola_id_new)} WHERE Assiste.id = {int(id)} AND Assiste.fk_aluno_id = {int(fk_aluno_id)} AND Assiste.fk_professor_id = {int(fk_professor_id)} AND Assiste.fk_escola_id = {int(fk_escola_id)};"
                 cursor.execute(sql)
                 mydb.commit()
-                st.success("Record Updated Successfully!!!")
+                st.success("Atualizado com SUCESSO!")
 
         elif table == "Concorre":
             fk_aluno_id = st.number_input("Antigo Id do Aluno", min_value=1)
@@ -489,7 +489,7 @@ def main():
                 sql = f"UPDATE Concorre SET Concorre.fk_aluno_id = {int(fk_aluno_id_new)}, Concorre.fk_olimpiada_id = {int(fk_olimpiada_id_new)} WHERE Concorre.id = {int(id)} AND Concorre.fk_aluno_id = {int(fk_aluno_id)} AND Concorre.fk_olimpiada_id = {int(fk_olimpiada_id)};"
                 cursor.execute(sql)
                 mydb.commit()
-                st.success("Record Updated Successfully!!!")
+                st.success("Atualizado com SUCESSO!")
 
         elif table == "Olimpiada_Disciplina":
             fk_olimpiada_id = st.number_input("Antigo Id da Olimpíada", min_value=1)
@@ -500,7 +500,7 @@ def main():
                 sql = f"UPDATE Olimpiada_Disciplina SET Olimpiada_Disciplina.fk_olimpiada_id = {int(fk_olimpiada_id_new)}, Olimpiada_Disciplina.fk_disciplina_id = {int(fk_disciplina_id_new)} WHERE Olimpiada_Disciplina.id = {int(id)} AND Olimpiada_Disciplina.fk_olimpiada_id = {int(fk_olimpiada_id)} AND Olimpiada_Disciplina.fk_disciplina_id = {int(fk_disciplina_id)};"
                 cursor.execute(sql)
                 mydb.commit()
-                st.success("Record Updated Successfully!!!")
+                st.success("Atualizado com SUCESSO!")
 
 
 
@@ -518,7 +518,7 @@ def main():
                 sql = f"DELETE FROM Usuario WHERE Usuario.id = {int(id)};"
                 cursor.execute(sql)
                 mydb.commit()
-                st.success("Record Deleted Successfully!!!")
+                st.success("Deletado com SUCESSO!")
 
         elif table == "Aluno":
             id = st.number_input("Id do Aluno", min_value= 1)
@@ -533,7 +533,7 @@ def main():
                 sql = f"DELETE FROM Usuario WHERE Usuario.id = {int(id)};"
                 cursor.execute(sql)
                 mydb.commit()
-                st.success("Record Deleted Successfully!!!")
+                st.success("Deletado com SUCESSO!")
 
         elif table == "Grupo":
             id = st.number_input("Id do Grupo", min_value= 1)
@@ -541,7 +541,7 @@ def main():
                 sql = f"DELETE FROM Grupo WHERE Grupo.id = {int(id)};"
                 cursor.execute(sql)
                 mydb.commit()
-                st.success("Record Deleted Successfully!!!")
+                st.success("Deletado com SUCESSO!")
 
         elif table == "Disciplina":
             id = st.number_input("Id da Disciplina", min_value= 1)
@@ -549,7 +549,7 @@ def main():
                 sql = f"DELETE FROM Disciplina WHERE Disciplina.id = {int(id)};"
                 cursor.execute(sql)
                 mydb.commit()
-                st.success("Record Deleted Successfully!!!")      
+                st.success("Deletado com SUCESSO!")      
 
         elif table == "Escola":
             id = st.number_input("Id da Escola", min_value= 1)
@@ -557,7 +557,7 @@ def main():
                 sql = f"DELETE FROM Escola WHERE Escola.id = {int(id)};"
                 cursor.execute(sql)
                 mydb.commit()
-                st.success("Record Deleted Successfully!!!")  
+                st.success("Deletado com SUCESSO!")  
 
         elif table == "Olimpiada":
             id = st.number_input("Id da Olimpiada", min_value= 1)
@@ -565,7 +565,7 @@ def main():
                 sql = f"DELETE FROM Olimpiada WHERE Olimpiada.id = {int(id)};"
                 cursor.execute(sql)
                 mydb.commit()
-                st.success("Record Deleted Successfully!!!")  
+                st.success("Deletado com SUCESSO!")  
 
         elif table == "Post":
             id = st.number_input("Id da Post", min_value= 1)
@@ -573,7 +573,7 @@ def main():
                 sql = f"DELETE FROM Post WHERE Post.id = {int(id)};"
                 cursor.execute(sql)
                 mydb.commit()
-                st.success("Record Deleted Successfully!!!")  
+                st.success("Deletado com SUCESSO!")  
 
         elif table == "Comentarios":
             id = st.number_input("Id do comentario", min_value= 1)
@@ -581,7 +581,7 @@ def main():
                 sql = f"DELETE FROM Comentarios WHERE Comentarios.id = {int(id)};"
                 cursor.execute(sql)
                 mydb.commit()
-                st.success("Record Deleted Successfully!!!")  
+                st.success("Deletado com SUCESSO!")  
 
         elif table == "Participa":
             fk_usuario_id = st.number_input("Id do usuário", min_value= 1)
@@ -590,7 +590,7 @@ def main():
                 sql = f"DELETE FROM Participa WHERE fk_usuario_id = {int(fk_usuario_id)} AND fk_grupo_id = {int(fk_grupo_id)};"
                 cursor.execute(sql)
                 mydb.commit()
-                st.success("Record Deleted Successfully!!!")  
+                st.success("Deletado com SUCESSO!")  
 
         elif table == "Grupo_Disciplina":
             fk_grupo_id = st.number_input("Id do grupo", min_value= 1)
@@ -599,7 +599,7 @@ def main():
                 sql = f"DELETE FROM grupo_disciplina WHERE fk_disciplina_id = {int(id_subject)} AND fk_grupo_id = {int(fk_grupo_id)};"
                 cursor.execute(sql)
                 mydb.commit()
-                st.success("Record Deleted Successfully!!!") 
+                st.success("Deletado com SUCESSO!") 
 
         elif table == "Prof_Disciplina":
             fk_usuario_id = st.number_input("Id do professor", min_value= 1)
@@ -608,7 +608,7 @@ def main():
                 sql = f"DELETE FROM Prof_Disciplina WHERE fk_disciplina_id = {int(id_subject)} AND fk_professor_id = {int(fk_usuario_id)};"
                 cursor.execute(sql)
                 mydb.commit()
-                st.success("Record Deleted Successfully!!!") 
+                st.success("Deletado com SUCESSO!") 
 
 ######
     
@@ -619,7 +619,7 @@ def main():
                 sql = f"DELETE FROM leciona WHERE fk_professor_id = {int(fk_professor_id)} AND fk_escola_id = {int(fk_escola_id)};"
                 cursor.execute(sql)
                 mydb.commit()
-                st.success("Record Deleted Successfully!!!")
+                st.success("Deletado com SUCESSO!")
 
     
         elif table == "Assiste":
@@ -630,7 +630,7 @@ def main():
                 sql = f"DELETE FROM assiste WHERE fk_aluno_id = {int(fk_aluno_id)} AND fk_professor_id = {int(fk_professor_id)} AND fk_escola_id = {int(fk_escola_id)};"
                 cursor.execute(sql)
                 mydb.commit()
-                st.success("Record Deleted Successfully!!!")
+                st.success("Deletado com SUCESSO!")
 
     
         elif table == "Concorre":
@@ -640,7 +640,7 @@ def main():
                 sql = f"DELETE FROM concorre WHERE fk_aluno_id = {int(fk_aluno_id)} AND fk_olimpiada_id = {int(fk_olimpiada_id)};"
                 cursor.execute(sql)
                 mydb.commit()
-                st.success("Record Deleted Successfully!!!")
+                st.success("Deletado com SUCESSO!")
 
 
         elif table == "Olimpiada_Disciplina":
@@ -650,7 +650,7 @@ def main():
                 sql = f"DELETE FROM olimpiada_disciplina WHERE fk_olimpiada_id = {int(fk_olimpiada_id)} AND fk_disciplina_id = {int(fk_disciplina_id)};"
                 cursor.execute(sql)
                 mydb.commit()
-                st.success("Record Deleted Successfully!!!")
+                st.success("Deletado com SUCESSO!")
 
 
 if __name__ == "__main__":
